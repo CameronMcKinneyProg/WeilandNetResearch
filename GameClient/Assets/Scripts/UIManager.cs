db418c01f2657e6752bfba6c3f197ac7c98e165e
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-21 16:14:03 +0000

    Add InputFieldUsername (Final Commit of Part 1)
    also change UIManager.usernameField type from InputField to TMP_InputField

--- GameClient/Assets/Scripts/UIManager.cs
+++ GameClient/Assets/Scripts/UIManager.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance; // singleton
 
     [SerializeField] private GameObject startMenu;
-    [SerializeField] private InputField usernameField;
+    [SerializeField] private TMP_InputField usernameField;
 
     private void Awake()
     {
