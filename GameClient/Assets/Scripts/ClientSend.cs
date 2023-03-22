8d302e9c5289f656d7ab772a8e18f977ea83b143
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-22 15:59:29 +0000

    Update GameClient scripts to send data to server

--- GameClient/Assets/Scripts/ClientSend.cs
+++ GameClient/Assets/Scripts/ClientSend.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class ClientSend : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static void SendTCPData(Packet _packet)
     {
-        
+        _packet.WriteLength();
+        Client.instance.tcp.SendData(_packet);
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Packets
+    public static void WelcomeReceived()
     {
-        
+        using (Packet _packet = new Packet((int)ClientPackets.welcomeReceived))
+        {
+            _packet.Write(Client.instance.myId);
+            _packet.Write(UIManager.instance.usernameField.text);
+
+            SendTCPData(_packet);
+        }
     }
+    #endregion
 }
