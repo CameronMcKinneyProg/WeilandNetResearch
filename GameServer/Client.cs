fdb31f077f55adcb246949bc59dcc3995ff8173a
Cameron McKinney <127255636+CameronMcKinneyProg@users.noreply.github.com>
2023-03-24 22:01:48 +0000

    Revert "Update ServerSend.cs and Client.cs"
    This reverts commit 1925ea92d50b8085a0e2e74663c46f154cda0d58.

--- GameServer/Client.cs
+++ GameServer/Client.cs
@@ -44,22 +44,7 @@ namespace GameServer
 
                 stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
 
-                ServerSend.Welcome(id, "Welcome to the server!");
-            }
-
-            public void SendData(Packet _packet)
-            {
-                try
-                {
-                    if (socket != null)
-                    {
-                        stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
-                    }
-                }
-                catch (Exception _ex)
-                {
-                    Console.WriteLine($"Error sending data to player {id} via TCP: {_ex}");
-                }
+                // TODO: send welcome packet
             }
 
             private void ReceiveCallback(IAsyncResult _result)
