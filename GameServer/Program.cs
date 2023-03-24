e8d7afd75340fd03815cf342a231ae38b25cec8d
Cameron McKinney <127255636+CameronMcKinneyProg@users.noreply.github.com>
2023-03-24 22:01:48 +0000

    Revert "Update Program.cs"
    This reverts commit 58096dd30ec89980374d30370cbd241084beecb1.

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -1,29 +1,16 @@
 ﻿using System;
-using System.Threading;
 
 namespace GameServer
 {
     class Program
     {
-        private static bool isRunning = false;
-
         static void Main(string[] args)
         {
             Console.Title = "Game Server";
-            isRunning = true;
-
-            Thread mainThread = new Thread(new ThreadStart(MainThread));
-            mainThread.Start();
 
             Server.Start(50, 29950);
-        }
 
-        /// <summary>
-        /// Runs the game loop.
-        /// </summary>
-        private static void MainThread()
-        {
-            Console.WriteLine("Main thread started. Running at {} ticks per second.");
+            Console.ReadKey();
         }
     }
 }
