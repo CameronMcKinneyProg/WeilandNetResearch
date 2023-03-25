af33e3445584211eeabae5658c0cdec7f16d8392
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-25 20:25:52 +0000

    Update Client.cs and Packet.cs

--- GameServer/Client.cs
+++ GameServer/Client.cs
@@ -154,7 +154,6 @@ namespace GameServer
             public void Connect(IPEndPoint _endPoint)
             {
                 endPoint = _endPoint;
-                ServerSend.UDPTest(id);
             }
 
             public void SendData(Packet _packet)
