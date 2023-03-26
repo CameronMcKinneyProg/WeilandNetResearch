5b7627508ebb49855923f0f2ef767dadc11d87f4
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-26 18:00:05 +0100

    Update Player.cs

--- UnityGameServer/Assets/Scripts/Player.cs
+++ UnityGameServer/Assets/Scripts/Player.cs
@@ -7,18 +7,13 @@ public class Player : MonoBehaviour
     public int id;
     public string username;
 
-    public Vector3 position;
-    public Quaternion rotation;
-
     private float moveSpeed = 5f / Constants.TICKS_PER_SEC;
     private bool[] inputs;
 
-    public Player(int _id, string _username, Vector3 _spawnPosition)
+    public Player(int _id, string _username)
     {
         id = _id;
         username = _username;
-        position = _spawnPosition;
-        rotation = Quaternion.identity;
 
         inputs = new bool[4];
     }
@@ -48,11 +43,8 @@ public class Player : MonoBehaviour
 
     private void Move(Vector2 _inputDirection)
     {
-        Vector3 _forward = Vector3.Transform(new Vector3(0, 0, 1), rotation);
-        Vector3 _right = Vector3.Normalize(Vector3.Cross(_forward, new Vector3(0, 1, 0)));
-
-        Vector3 _moveDirection = _right * _inputDirection.x + _forward * _inputDirection.x;
-        position += _moveDirection * moveSpeed;
+        Vector3 _moveDirection = transform.right * _inputDirection.x + transform.forward * _inputDirection.x;
+        transform.position += _moveDirection * moveSpeed;
 
         ServerSend.PlayerPosition(this);
         ServerSend.PlayerRotation(this);
