6750d6df078edc3a303b805a87910d2ed985f75e
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-03-26 17:50:07 +0100

    Copy Constants.cs

--- UnityGameServer/Assets/Scripts/Constants.cs
+++ UnityGameServer/Assets/Scripts/Constants.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Constants : MonoBehaviour
+public class Constants
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public const int TICKS_PER_SEC = 30;
+    public const int MS_PER_TICK = 1000 / TICKS_PER_SEC;
 }
