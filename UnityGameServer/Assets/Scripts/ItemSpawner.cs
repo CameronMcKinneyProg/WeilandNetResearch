bb4387516782b6ee591dfd398b49b2bb17e7f125
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-04-12 22:04:29 +0100

    Update ItemSpawner.cs

--- UnityGameServer/Assets/Scripts/ItemSpawner.cs
+++ UnityGameServer/Assets/Scripts/ItemSpawner.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ItemSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static Dictionary<int, ItemSpawner> spawners = new Dictionary<int, ItemSpawner>();
+    private static int nextSpawnerId = 1;
+
+    public int spawnerId;
+    public bool hasItem = false;
+
+    private void Start()
     {
-        
+        hasItem = false;
+        spawnerId = nextSpawnerId;
+        nextSpawnerId++;
+        spawners.Add(spawnerId, this);
+
+        StartCoroutine(SpawnItem());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            Player _player = other.GetComponent<Player>();
+            if (_player.AttemptPickupItem())
+            {
+                ItemPickedUp(_player.id);
+            }
+        }
+    }
+
+    private IEnumerator SpawnItem()
+    {
+        yield return new WaitForSeconds(10f);
+
+        hasItem = true;
+    }
+
+    private void ItemPickedUp(int _byPlayer)
+    {
+        hasItem = false;
+
+        StartCoroutine(SpawnItem());
     }
 }
