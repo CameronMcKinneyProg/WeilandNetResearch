94422460befbf838e45e2acb287cd5869cc9db38
CameronMcKinneyProg <cameronmckinneyprog@gmail.com>
2023-04-12 22:16:17 +0100

    Add createItemSpawner packet to Packet.cs

--- UnityGameServer/Assets/Scripts/Packet.cs
+++ UnityGameServer/Assets/Scripts/Packet.cs
@@ -13,7 +13,8 @@ public enum ServerPackets
     playerRotation,
     playerDisconnected,
     playerHealth,
-    playerRespawned
+    playerRespawned,
+    createItemSpawner
 }
 
 /// <summary>Sent from client to server.</summary>
